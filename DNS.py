5737d90c33c3cbb0e2b7bab9ebe812809e5641dd
katsuya <katu7414@gmail.com>
2019-05-16 16:54:03 +0900

    Update DNS.py

--- DNS.py
+++ DNS.py
@@ -4,6 +4,7 @@ cap = pyshark.LiveCapture(interface='en0', bpf_filter='udp port 53')
 
 cap.sniff(packet_count=10)
 
+
 def print_dns_info(pkt):
     if pkt.dns.qry_name:
         print 'DNS Request from %s: %s' % (pkt.ip.src, pkt.dns.qry_name)
